80c5f1bb40013c02cf3574228367e7b847ac41eb
devsafix <devsafix@gmail.com>
2025-05-25 01:34:21 +0600

    Rangers table created

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,2 +1,9 @@
 -- Active: 1747412702955@@localhost@5432@conservation_db
 CREATE DATABASE conservation_db;
+
+-- Rangers Table
+CREATE TABLE rangers (
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    region VARCHAR(50) NOT NULL
+);
