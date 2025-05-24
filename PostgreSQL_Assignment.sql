a8cd57cb1be5bcecacac7215139b3fea1995ed0d
devsafix <devsafix@gmail.com>
2025-05-25 01:38:57 +0600

    Species table created

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -7,3 +7,19 @@ CREATE TABLE rangers (
     name VARCHAR(50) NOT NULL,
     region VARCHAR(50) NOT NULL
 );
+
+-- Species Table
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(50) NOT NULL,
+    scientific_name VARCHAR(50) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(50) CHECK (
+        conservation_status IN (
+            'Endangered',
+            'Vulnerable',
+            'Near Threatened',
+            'Least Concern'
+        )
+    )
+);
