0abc82a2ea107ef63aacd08164c622d007e04b73
devsafix <devsafix@gmail.com>
2025-05-25 12:37:21 +0600

    solve - problem 9

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -71,3 +71,11 @@ SELECT
         ELSE 'Evening'
     END AS time_of_day
 FROM sightings;
+
+-- Problem 9
+DELETE FROM rangers
+WHERE
+    ranger_id NOT IN (
+        SELECT ranger_id
+        FROM sightings
+    );
